a060d2771e320d00e792bc86b7f0421e0dd6747d
jung <agppg@naver.com>
2024-09-10 19:47:10 +0900

    swagger 연결 완료

---
+++ src/main/java/codeview/main/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package codeview.main.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    private static final String BEARER_TOKEN_PREFIX = "Bearer";
+
+    @Bean
+    public OpenAPI openAPI(){
+        String securityJwtName = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+
+
+        Components components = new Components()
+                .addSecuritySchemes(securityJwtName, new SecurityScheme()
+                        .name(securityJwtName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(BEARER_TOKEN_PREFIX)
+                        .bearerFormat(securityJwtName));
+
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("codeView API");
+
+
+        return new OpenAPI()
+                .components(components)
+                .info(info);
+    }
+
+}
