f28c556a6b9534df89b8f124f604873cdc3b6086
BluIsMe <soeun2388@kangnam.ac.kr>
2024-09-25 11:52:46 +0900

    CustomOAuthUserService 일부 수정

--- src/main/java/codeview/main/dto/user/UserInfoResponseDto.java
+++ src/main/java/codeview/main/dto/user/UserInfoResponseDto.java
@@ -2,8 +2,10 @@ package codeview.main.dto.user;
 
 import codeview.main.entity.User;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserInfoResponseDto {
     private Long id;
     private String email;
